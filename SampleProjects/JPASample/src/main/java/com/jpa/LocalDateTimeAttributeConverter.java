c774d9307381a7f09fe277dafc9ab9c350dfabe9
559207 <559207@PC334044.cts.com>
2016-09-13 15:56:23 +0530

    Added comments to Converter classes.

--- SampleProjects/JPASample/src/main/java/com/jpa/LocalDateTimeAttributeConverter.java
+++ SampleProjects/JPASample/src/main/java/com/jpa/LocalDateTimeAttributeConverter.java
@@ -5,7 +5,30 @@ import java.time.LocalDateTime;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+/**
+ * The same will work with javax.persistence version 2.1 and hibernate version 4.3.2.Final and higher.
+ * 
+ *               <dependency>
+ *                    <groupId>org.eclipse.persistence</groupId>
+ *                     <artifactId>javax.persistence</artifactId>
+ *                   <version>2.1.0</version>
+ *          </dependency>
+ *             <dependency>
+ *                    <groupId>org.hibernate</groupId>
+ *                    <artifactId>hibernate-entitymanager</artifactId>
+ *                    <version>4.3.2.Final</version>
+ *             </dependency>
+ *          
+ *          http://www.thoughts-on-java.org/persist-localdate-localdatetime-jpa/
+ *
+ * 			http://stackoverflow.com/questions/36363913/unable-to-use-java-time-localdate-in-jpa-entity-with-mysql
+ *
+ *          http://stackoverflow.com/questions/28897303/persist-java-localdate-in-mysql
 
+ *
+ * @author krishna
+ *
+ */
 @Converter(autoApply = true)
 public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
